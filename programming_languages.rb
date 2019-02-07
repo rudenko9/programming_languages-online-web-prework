a0f60f771174e1703006c03c95444c7a3f928da5
Nataliya R <n.rudenko@gmail.com>
2019-02-07 01:41:44 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,8 +2,8 @@ def reformat_languages(languages)
   new_hash = {}
   language_hash.each do |k, v|
     v.each do |k1, v1|
-      new_hash[k1] || = v1
-      new_hash[k1][:style] || = []
+      new_hash[k1] ||= v1
+      new_hash[k1][:style] ||= []
       new_hash[k1][:style] << k
     end
   end
