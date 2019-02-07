aa62bd674fe053ee1ccef9e08b56403965320606
Nataliya R <n.rudenko@gmail.com>
2019-02-07 01:53:13 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,44 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |key, value|
+    value.each do |key1, value1|
+      new_hash[key1] ||= value1
+      new_hash[key1][:style] ||= []
+      new_hash[key1][:style] << key
+    end
+  end
+  new_hash
 end
+
